8e6872050806a7bb2181a0a42f53e47c47c3ab9f
GLPetroni <60444067+GLPetroni@users.noreply.github.com>
2021-07-23 15:42:10 -0700

    Delete public directory

--- public/Sslider.js
+++
@@ -1,7 +0,0 @@
-var Sslider = document.getElementById("Sslide");
-var Soutput = document.getElementById("sdude");
-Soutput.innerHTML = Sslider.value;
-
-Sslider.oninput = function() {
-  Soutput.innerHTML = this.value;
-}
